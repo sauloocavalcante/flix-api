8dbf1bd960f9bb0943a996872663dc94333a0211
sauloocavalcante <saulolvl1@gmail.com>
2025-02-19 15:50:56 -0300

    Updated genres module: added serializers and modified views and URL configurations

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_create_list_view, genre_detail_view
+from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_create_list_view, name='genre'),
-    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
+    path('genres/', GenreCreateListView.as_view(), name='genre'),
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
 ]
