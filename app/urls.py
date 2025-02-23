5fa64951efbc917cfa971cd8b0e4dacd33eca530
sauloocavalcante <saulolvl1@gmail.com>
2025-02-23 00:49:17 -0300

    New app created: authentication and implemented jwt authentication

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
+
 
 urlpatterns = [
      path('admin/', admin.site.urls),
 
+     path('api/v1/', include('authentication.urls')),
      path('api/v1/', include('genres.urls')),
      path('api/v1/', include('actors.urls')),
      path('api/v1/', include('movies.urls')),
